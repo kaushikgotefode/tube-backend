34eaef9830e0331ece6e263e0d0e74e9bb75c2cc
Kaushik Gotefode <kaushikgotefode@gmail.com>
2025-04-29 17:53:04 +0530

    database connection with mongoose

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+dotenv.config({path: './.env'});
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}
-);
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-}
-);
+connectDB();
 
